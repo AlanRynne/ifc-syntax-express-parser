cd4aad926d8ea60287d6a304657ca03b49815209
Alan Rynne <alan@rynne.es>
2019-07-24 19:22:47 +0200

    First commit Working express parser (without validation or function codes)

---
+++ IFC4x2.Net/ExpressBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IFC4x2.Net
+{
+    public class ExpressBase<T>
+    {
+        public T Value;
+
+        public ExpressBase(T value){
+            Value = value;
+        }
+
+        public static implicit operator T(ExpressBase<T> e) => e.Value;
+        public static implicit operator ExpressBase<T>(T b) => new ExpressBase<T>(b); 
+
+    }
+
+    #region Primitives
+
+    public class eString : ExpressBase<string>
+    {
+        private bool Fixed;
+        private double? Length; 
+        public eString(string value) : base(value)
+        {
+        }
+    }
+    public class eDouble : ExpressBase<double>
+    {
+        public eDouble(double value) : base(value)
+        {
+        }
+    }
+    public class eInteger : ExpressBase<int>
+    {
+        public eInteger(int value) : base(value)
+        {
+        }
+    }    
+    public class eBoolean : ExpressBase<bool>
+    {
+        public eBoolean(bool value) : base(value)
+        {
+        }
+    }
+
+    #endregion
+
+    #region Collections
+
+    public class eList<T> : ExpressBase<List<T>>
+    {
+        public eList(List<T> value) : base(value)
+        {
+        }
+    }
+    public class eSet<T> : ExpressBase<List<T>>
+    {
+        public eSet(List<T> value) : base(value)
+        {
+        }
+    }
+    public class eArray<T> : ExpressBase<T[]>
+    {
+        public eArray(T[] value) : base(value)
+        {
+        }
+    }
+    public class eBag<T> : ExpressBase<T[]>
+    {
+        public eBag(T[] value) : base(value)
+        {
+        }
+    }
+
+    #endregion
+
+}
---
+++ index.js
@@ -0,0 +1,52 @@
+const nearley = require("nearley");
+const grammar = require("./expressGrammar.js");
+const fs = require('fs');
+const readline = require('readline');
+
+
+let parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+
+let jsonObj = {
+    schema: null,
+    header: [],
+    types: {},
+    entities: {},
+    functions: {},
+    rules: {}
+};
+const rl = readline.createInterface({
+    input: fs.createReadStream('./Data/testExpress.exp'),
+    crlfDelay: Infinity
+});
+
+rl.on('line', (l) => {
+    line = l.trim();
+    //console.log(line);
+    parser.feed(line);
+    if (parser.results.length != 0) {
+        if (parser.results[0] != null) {
+            expEnt = parser.results[0];
+            if (expEnt.ifcType == "type") jsonObj.types[expEnt.name] = expEnt;
+            else if (expEnt.ifcType == "entity") jsonObj.entities[expEnt.name] = expEnt;
+            else if (expEnt.ifcType == "function") jsonObj.functions[expEnt.name] = expEnt;
+            else if (expEnt.ifcType == "rule") jsonObj.rules[expEnt.name] = expEnt;
+            else if (expEnt.ifcType == "comment") jsonObj.header = expEnt.value;
+            else if (expEnt.name == "schema") jsonObj.schema = expEnt.version;
+            else jsonObj[expEnt.name] = expEnt;
+            parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+        }
+    } else if (parser.results.length > 1) {
+        console.log("AMBIGUOUS LINE! Line" + line);
+    } else {
+    }
+});
+rl.on('close', () => {
+    fs.writeFile(
+        'testExpress.json',
+        JSON.stringify(jsonObj, null, 2),
+        (err) => {
+            // In case of a error throw err. 
+            if (err) throw err;
+        })
+    console.log('Finished writting')
+});
